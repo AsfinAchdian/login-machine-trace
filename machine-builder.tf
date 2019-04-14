a4c9e77fa68bed610c453f15036b6fc5b5c9b0df
asfin achdian <asfin.achdian@mekar.id>
2019-04-14 23:06:43 +0700

    add login-trace file

--- machine-builder.tf
+++ machine-builder.tf
@@ -1,4 +1,4 @@
-# Create a new instance of the latest Ubuntu 14.04 on an
+# Create first instance of the latest Ubuntu 14.04 on an
 # t2.micro node with an AWS Tag naming it "nodeABC"
 
 provider "aws" {
@@ -18,7 +18,7 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["karyaanakbangsa"] # Canonical
+  owners = ["KaryaAnakBangsa"] # Canonical
 }
 
 resource "aws_instance" "web" {
@@ -27,5 +27,37 @@ resource "aws_instance" "web" {
 
   tags = {
     Name = "nodeABC"
+  }
+}
+
+# Create second instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "nodeXYZ"
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["KaryaAnakBangsa"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "nodeXYZ"
   }
 }
